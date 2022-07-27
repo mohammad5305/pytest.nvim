55e3c51520d03bb639a8681700c1533aecfc2dac
mohammad5305 <mohammad.bajalal546@gmail.com>
2022-07-27 21:44:08 +0430

    feat: using setup function

--- lua/pytest.lua
+++ lua/pytest.lua
@@ -1,12 +1,22 @@
 -- just for readablity I use this var
 local snippet = require("pytest.snippet")
+local pytest = {}
+local defualtOpts = {}
 
-vim.api.nvim_create_user_command("PytestMkSnip", function ()
-    local bufnum = vim.api.nvim_buf_get_number(0)
-    snippet.insertSnippet(bufnum, "n")
-end,{})
+function pytest.setup(opts)
+    if opts ~= nil then
+        opts = defualtOpts
+    end
 
-vim.api.nvim_create_user_command("PytestMkSnip", function ()
-    local bufnum = vim.api.nvim_buf_get_number(0)
-    snippet.insertSnippet(bufnum, "v")
-end, { range=true })
+    vim.api.nvim_create_user_command("PytestMkSnip", function ()
+        local bufnum = vim.api.nvim_buf_get_number(0)
+        snippet.insertSnippet(bufnum, "n")
+    end,{})
+
+    vim.api.nvim_create_user_command("PytestMkSnip", function ()
+        local bufnum = vim.api.nvim_buf_get_number(0)
+        snippet.insertSnippet(bufnum, "v")
+    end, { range=true })
+end
+
+return pytest
