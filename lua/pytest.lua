c1e1932742953a759fa3d63043992ae81b31c5d3
mohammad5305 <mohammad.bajalal546@gmail.com>
2022-08-01 14:38:52 +0430

    ref: using test_filename and moving is_dir to helper

--- lua/pytest.lua
+++ lua/pytest.lua
@@ -1,13 +1,11 @@
 -- just for readablity I use this var
 local snippet = require("pytest.snippet")
+local helper = require("pytest.helper")
 local pytest = {}
 local defualtOpts = {
     testDir="tests/"
 }
 
-function is_dir(path)
-  return os.execute(('[ -d "%s" ]'):format(path))
-end
 
 function pytest.setup(opts)
     if opts == nil then
@@ -15,9 +13,10 @@ function pytest.setup(opts)
     end
 
         vim.api.nvim_create_user_command("PytestMkSnip", function ()
-            if is_dir(vim.loop.cwd()..'/'..opts['testDir']) == 0 then
+            if helper.is_dir(vim.loop.cwd()..'/'..opts['testDir']) == 0 then
                 local bufnr = vim.api.nvim_buf_get_number(0)
-                snippet.insertSnippet(bufnr, "n", opts['testDir'], string.gsub(vim.api.nvim_buf_get_name(0), vim.loop.cwd()..'/', ''))
+                local filename = string.gsub(vim.api.nvim_buf_get_name(0), vim.loop.cwd()..'/', '')
+                snippet.insertSnippet(bufnr, "n", opts['testDir'], 'test_'..filename)
             else
                 -- TODO: getting answar from user
                 print(string.format("%s direcotry not exists in current path should I make?", opts['testDir']))
@@ -26,9 +25,10 @@ function pytest.setup(opts)
         end, {})
 
         vim.api.nvim_create_user_command("PytestMkSnipV", function ()
-            if is_dir(vim.loop.cwd()..'/'..opts['testDir']) == 1 then
+            if helper.is_dir(vim.loop.cwd()..'/'..opts['testDir']) == 1 then
                 local bufnr = vim.api.nvim_buf_get_number(0)
-                snippet.insertSnippet(bufnr, "v", opts['testDir'], string.gsub(vim.api.nvim_buf_get_name(0), vim.loop.cwd()..'/', ''))
+                local filename = string.gsub(vim.api.nvim_buf_get_name(0), vim.loop.cwd()..'/', '')
+                snippet.insertSnippet(bufnr, "v", opts['testDir'], 'test_'..filename)
             else
                 -- TODO: getting answar from user
                 print(string.format("%s direcotry not exists in current path should I make?", opts['testDir']))
--- lua/pytest/helper.lua
+++ lua/pytest/helper.lua
@@ -1,4 +1,8 @@
 local helper = {}
 
+function helper.is_dir(path)
+    return os.execute(('[ -d "%s" ]'):format(path))
+end
+
 
 return helper
