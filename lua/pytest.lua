94fd52e8f112dadb58f0169eefddf3eae0bd7dfa
mohammad5305 <mohammad.bajalal546@gmail.com>
2022-08-02 22:30:23 +0430

    feat: better indent and asking from user if opts[testDir] not exist

--- lua/pytest.lua
+++ lua/pytest.lua
@@ -12,28 +12,41 @@ function pytest.setup(opts)
         opts = defualtOpts
     end
 
-        vim.api.nvim_create_user_command("PytestMkSnip", function ()
-            if helper.is_dir(vim.loop.cwd()..'/'..opts['testDir']) == 0 then
-                local bufnr = vim.api.nvim_get_current_buf()
-                snippet.insertSnippet(bufnr, "n", opts['testDir'], 'test_'..vim.fn.expand('%'))
-            else
-                -- TODO: getting answar from user
-                print(string.format("%s direcotry not exists in current path should I make?", opts['testDir']))
-            end
+    vim.api.nvim_create_user_command("PytestMkSnip", function ()
+        local bufnr = vim.api.nvim_get_current_buf()
+        if helper.is_dir(vim.loop.cwd()..'/'..opts['testDir']) == 0 then
+            snippet.insertSnippet(bufnr, "n", opts['testDir'], 'test_'..vim.fn.expand('%'))
+        else
+            vim.ui.select({ string.format('create %s', opts['testDir']), 'custom dir name' },
+                {prompt=string.format("%s no such directory in current path: ", opts['testDir'])},
+                function(choice, number) 
+                    vim.cmd("redraw")
+                    if number == 1 then
+                        vim.fn.mkdir(opts['testDir'])
+                        snippet.insertSnippet(bufnr, "n", opts['testDir'], 'test_'..vim.fn.expand('%'))
 
-        end, {})
+                    elseif number == 2 then
+                        vim.ui.input({prompt="directory name: "}, function(dirName)
+                            snippet.insertSnippet(bufnr, "n", string.find(dirName,'/') and dirName or dirName..'/' , 'test_'..vim.fn.expand('%'))
+                        end)
+                    end
+                end)
 
-        vim.api.nvim_create_user_command("PytestMkSnipV", function ()
-            if helper.is_dir(vim.loop.cwd()..'/'..opts['testDir']) == 1 then
-                local bufnr = vim.api.nvim_get_current_buf()
-                snippet.insertSnippet(bufnr, "v", opts['testDir'], 'test_'..vim.fn.expand('%'))
-            else
-                -- TODO: getting answar from user
-                print(string.format("%s direcotry not exists in current path should I make?", opts['testDir']))
-            end
-        end, { range=true })
+        end
+
+    end, {})
+
+    vim.api.nvim_create_user_command("PytestMkSnipV", function ()
+        if helper.is_dir(vim.loop.cwd()..'/'..opts['testDir']) == 1 then
+            local bufnr = vim.api.nvim_get_current_buf()
+            snippet.insertSnippet(bufnr, "v", opts['testDir'], 'test_'..vim.fn.expand('%'))
+        else
+            -- TODO: getting answar from user
+            print(string.format("%s direcotry not exists in current path should I make?", opts['testDir']))
+        end
+    end, { range=true })
 
 
-    end
+end
 
-    return pytest
+return pytest
