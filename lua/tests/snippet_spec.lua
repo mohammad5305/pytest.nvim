089d80640e6d9a42d563cc7313b7dec69d0f0050
mohammad5305 <mohammad.bajalal546@gmail.com>
2022-08-10 15:07:28 +0430

    test: multi function support getQuery

--- lua/tests/snippet_spec.lua
+++ lua/tests/snippet_spec.lua
@@ -25,7 +25,28 @@ describe("snippet", function()
             local docstringQuery = helper.getQuery('(function_definition body: (block (expression_statement (string)@capture)))', bufnr)[1]
             assert.equals(vim.treesitter.get_node_text(functioNameQuery, bufnr), "testFunc")
             assert.equals(vim.treesitter.get_node_text(docstringQuery, bufnr), '""" hello world"""')
+            vim.cmd("bd!")
+        end)
+
+        it("should get all function and docstring in file", function()
+            vim.cmd('e test.py')
+            local bufnr = vim.api.nvim_get_current_buf()
+            local functionName = {"testFunc", "hello_world", }
+            local docstring = {'""" testFunc docstring """', '""" hello world """'}
+
+            for i=1,#functionName do
+                vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, {string.format("def %s():", functionName[i]), "\t"..docstring[i], "\tpass"})
+            end
+
+            local functioNameQuery = helper.getQuery('(function_definition name: (identifier)@capture)', bufnr)
+            local docstringQuery = helper.getQuery('(function_definition body: (block (expression_statement (string)@capture)))', bufnr)
+            for i=1,#functioNameQuery do
+                assert.equals(vim.treesitter.get_node_text(functioNameQuery[i], bufnr), functionName[i])
+                assert.equals(vim.treesitter.get_node_text(docstringQuery[i], bufnr), docstring[i])
+            end
+            vim.cmd("bd!")
         end)
 
     end)
+
 end)
