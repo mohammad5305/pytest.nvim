11e795601521896e3d1efc1382aab9713c7947c4
mohammad5305 <mohammad.bajalal546@gmail.com>
2022-08-18 22:19:11 +0430

    style: 2 space indention instead of 4

--- lua/pytest.lua
+++ lua/pytest.lua
@@ -3,36 +3,36 @@ local snippet = require("pytest.snippet")
 local helper = require("pytest.helper")
 local pytest = {}
 local defualtOpts = {
-    testDir="tests/",
-    dirExistancePrmopt=nil,
+  testDir="tests/",
+  dirExistancePrmopt=nil,
 }
 
 
 function pytest.setup(opts)
-    if opts == nil then
-        opts = defualtOpts
+  if opts == nil then
+    opts = defualtOpts
+  end
+
+  vim.api.nvim_create_user_command("PytestMkSnip", function ()
+    local bufnr = vim.api.nvim_get_current_buf()
+    if helper.is_dir(vim.loop.cwd()..'/'..opts['testDir']) == 0 then
+      snippet.insertSnippet(bufnr, "n", opts['testDir'], 'test_'..vim.fn.expand('%'))
+    else
+      local createdDir = helper.createDir(opts['testDir'], opts['dirExistancePrmopt'])
+      snippet.insertSnippet(bufnr, "n", createdDir, 'test_'..vim.fn.expand('%'))
     end
 
-    vim.api.nvim_create_user_command("PytestMkSnip", function ()
-        local bufnr = vim.api.nvim_get_current_buf()
-            if helper.is_dir(vim.loop.cwd()..'/'..opts['testDir']) == 0 then
-                snippet.insertSnippet(bufnr, "n", opts['testDir'], 'test_'..vim.fn.expand('%'))
-            else
-                local createdDir = helper.createDir(opts['testDir'], opts['dirExistancePrmopt'])
-                snippet.insertSnippet(bufnr, "n", createdDir, 'test_'..vim.fn.expand('%'))
-            end
+  end, {})
 
-    end, {})
-
-    vim.api.nvim_create_user_command("PytestMkSnipV", function ()
-        if helper.is_dir(vim.loop.cwd()..'/'..opts['testDir']) == 1 then
-            local bufnr = vim.api.nvim_get_current_buf()
-            snippet.insertSnippet(bufnr, "v", opts['testDir'], 'test_'..vim.fn.expand('%'))
-        else
-            -- TODO: getting answar from user
-            print(string.format("%s direcotry not exists in current path should I make?", opts['testDir']))
-        end
-    end, { range=true })
+  vim.api.nvim_create_user_command("PytestMkSnipV", function ()
+    if helper.is_dir(vim.loop.cwd()..'/'..opts['testDir']) == 1 then
+      local bufnr = vim.api.nvim_get_current_buf()
+      snippet.insertSnippet(bufnr, "v", opts['testDir'], 'test_'..vim.fn.expand('%'))
+    else
+      -- TODO: getting answar from user
+      print(string.format("%s direcotry not exists in current path should I make?", opts['testDir']))
+    end
+  end, { range=true })
 
 
 end
--- lua/pytest/snippet.lua
+++ lua/pytest/snippet.lua
@@ -4,10 +4,10 @@ local snippet = {}
 local treesitter = vim.treesitter
 
 function snippet.makeSnippet(functionName, docstring)
-    docstring = string.gsub(docstring, '[\n|\t|"""]', "")
-    -- TODO: combin these regex
-    docstring = string.gsub(docstring, '(%s+)(%s*)(%s+)', "")
-    return string.format([[def test_%s():
+  docstring = string.gsub(docstring, '[\n|\t|"""]', "")
+  -- TODO: combin these regex
+  docstring = string.gsub(docstring, '(%s+)(%s*)(%s+)', "")
+  return string.format([[def test_%s():
     """ test %s """
     pass
 
@@ -15,43 +15,43 @@ function snippet.makeSnippet(functionName, docstring)
 end
 
 function snippet.insertSnippet(bufnr, mode, testDir, filename)
-    local functionNames =  helper.getQuery('(function_definition name: (identifier)@capture)', bufnr)
-    local docstrings = helper.getQuery('(function_definition body: (block (expression_statement (string)@capture)))', bufnr)
-    local snippetTables = {}
+  local functionNames =  helper.getQuery('(function_definition name: (identifier)@capture)', bufnr)
+  local docstrings = helper.getQuery('(function_definition body: (block (expression_statement (string)@capture)))', bufnr)
+  local snippetTables = {}
 
-    for i=1,#functionNames do
-        local functionName = functionNames[i]
-        local docstring = docstrings[i]
+  for i=1,#functionNames do
+    local functionName = functionNames[i]
+    local docstring = docstrings[i]
 
-        -- TODO: weird but working :)
-        if tonumber(tostring(functionName:start()))+1 == tonumber(tostring(docstring:start())) then
-            table.insert(snippetTables, vim.split(snippet.makeSnippet(
-                treesitter.get_node_text(functionName, bufnr),
-                treesitter.get_node_text(docstring, bufnr)
-            ), "\n"))
-        else
-            repeat 
-                k = i + 1
-                docstring = docstrings[k]
-            until rowFuncName+1 >= tonumber(tostring(docstring:start()))
+    -- TODO: weird but working :)
+    if tonumber(tostring(functionName:start()))+1 == tonumber(tostring(docstring:start())) then
+      table.insert(snippetTables, vim.split(snippet.makeSnippet(
+        treesitter.get_node_text(functionName, bufnr),
+        treesitter.get_node_text(docstring, bufnr)
+      ), "\n"))
+    else
+      repeat 
+        k = i + 1
+        docstring = docstrings[k]
+      until rowFuncName+1 >= tonumber(tostring(docstring:start()))
 
-            table.insert(snippetTables, vim.split(snippet.makeSnippet(
-                treesitter.get_node_text(functionName, bufnr),
-                treesitter.get_node_text(docstring, bufnr)
-            ), "\n"))
-        end
+      table.insert(snippetTables, vim.split(snippet.makeSnippet(
+        treesitter.get_node_text(functionName, bufnr),
+        treesitter.get_node_text(docstring, bufnr)
+      ), "\n"))
     end
-    vim.cmd('e '..testDir..filename)
-    local testFile = table.concat(vim.api.nvim_buf_get_lines(vim.api.nvim_get_current_buf(), 0, -1, false), "\n")
+  end
+  vim.cmd('e '..testDir..filename)
+  local testFile = table.concat(vim.api.nvim_buf_get_lines(vim.api.nvim_get_current_buf(), 0, -1, false), "\n")
 
-    for index, snippet in ipairs(snippetTables) do
-        local functionPattern = "def %w+_(.+):"
-        local _, _, snippetMatch = string.find(table.concat(snippet, "\n"), functionPattern)
+  for index, snippet in ipairs(snippetTables) do
+    local functionPattern = "def %w+_(.+):"
+    local _, _, snippetMatch = string.find(table.concat(snippet, "\n"), functionPattern)
 
-        if string.match(testFile, snippetMatch) == nil then
-            vim.api.nvim_buf_set_lines(vim.api.nvim_get_current_buf(), -1, -1, false, snippet)
-        end
+    if string.match(testFile, snippetMatch) == nil then
+      vim.api.nvim_buf_set_lines(vim.api.nvim_get_current_buf(), -1, -1, false, snippet)
     end
+  end
 
 end
 
