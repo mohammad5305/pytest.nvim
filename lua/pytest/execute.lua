982144532033ba374bda7e1012d57404336bce3c
mohammad5305 <mohammad.bajalal546@gmail.com>
2022-08-20 22:01:09 +0430

    feat: running pytest asynchronously added

---
+++ lua/pytest/execute.lua
@@ -0,0 +1,59 @@
+function setSignCol() end
+
+function findStatus(report, regex, index)
+  _, indexEnd, path, functionName, status = string.find(report, regex, index)
+  return {
+    ['path'] = path,
+    ['functionName'] = functionName,
+    ['status'] = status
+  }, indexEnd + 1
+end
+
+function processReport(report, times)
+  if times then
+    -- TODO: combine these 2 varibale into one line
+    local lastIndex = 0
+    local reportTbl = {}
+    local match = 0
+    for i = 1, times do
+      match, lastIndex = findStatus(report, "([^%s]+)::([^%s]+)(%s%w+)",
+        lastIndex)
+      table.insert(reportTbl, match)
+    end
+    return reportTbl
+  else
+    return findStatus(report, "([^%s]+)::([^%s]+)(%s%w+)")
+  end
+end
+
+function executePytest(cmd, cwd, async)
+  if async then
+    local times = nil
+    local function on_event(job_id, data, event)
+      if event == "stdout" or event == "stderr" then
+        if data then
+          local stringData = table.concat(data, "\n")
+          if times == nil then
+            _, _, times = string.find(stringData, "collected (%d+)")
+          end
+          -- TODO: the the output of this function and pipe it to setSignCol also code base getting so complicated
+          processReport(stringData, times)
+        end
+      end
+
+    end
+
+    local job_id = vim.fn.jobstart(cmd, {
+      cwd = cwd,
+      on_stdout = on_event,
+      stdout_buffered = true,
+      stderr_buffered = true
+    })
+    return result
+  else
+  end
+end
+
+executePytest(
+  "pytest -v --capture=no --no-summary --no-header --color=no  --disable-warnings | tr -d '='",
+  '/tmp/pytest-example/', true)
